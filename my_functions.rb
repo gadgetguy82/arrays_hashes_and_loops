b58ad1ffe6de6375d2cf3f45c889fa08c7c04992
Robert Tam <rsh.tam@gmail.com>
2019-05-15 15:51:04 +0100

    Add all functions and tests

--- my_functions.rb
+++ my_functions.rb
@@ -1,4 +1,28 @@
 def add_array_lengths(array1, array2)
-  length = array1.length + array2.lengths
+  length = array1.length + array2.length
   return length
+end
+
+def sum_array(array)
+  total = 0
+  for number in array
+    total += number
+  end
+
+  return total
+end
+
+def find_item(array, string)
+  for item in array
+    if (item == string)
+      return true
+    end
+  end
+  return false
+end
+
+def get_first_key(hash)
+  for item in hash.keys
+    return item
+  end
 end
